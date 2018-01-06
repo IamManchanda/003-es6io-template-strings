40f4704053ab9c166f425773bac807b792cb27ee
Harry Manchanda <harmanmanchanda182@gmail.com>
2018-01-07 00:18:11 +0530

    Creating HTML with Template Strings!

--- js/script.js
+++ js/script.js
@@ -2,7 +2,24 @@
  * JavaScript
  */
 
- const name = 'Snickers';
- const age = 2;
- const sentence = `My dog ${name} is ${age * 7} years old`;
-console.log(sentence);
+const person = {
+  name: 'Harry Manchanda',
+  job: 'Frontend Web Developer',
+  aim: 'FullStack Web & Mobile Apps Developer',
+  city: 'Delhi',
+  bio: 'Harry is a really studious guy who want to learn the best of the web.',
+};
+
+const markup = `
+  <div class="person">
+    <h3>
+      ${person.name}
+      <span class="job">${person.job}</span>
+    </h3>
+    <p>Aim to be a <span class="aim">${person.aim}</span></p>
+    <p class="location">He lives in ${person.city}</p>
+    <p class="bio">${person.bio}</p>
+  </div>
+`;
+
+document.body.innerHTML = markup;
